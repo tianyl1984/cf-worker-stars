32f47282cdd0890c9372bb34659d1b0b1b073732
tyl <tianyl1984@qq.com>
2025-02-08 16:17:17 +0800

    add debug

--- src/github.js
+++ src/github.js
@@ -46,11 +46,15 @@ async function getGist(token, gistId) {
 		method: 'GET',
 		headers: header,
 	});
-	return resp.json();
+	const text = await resp.text();
+	console(text);
+	return JSON.parse(text);
 }
 
 async function getGistContent(token, gistId) {
+	console.log(gistId);
 	const gist = await getGist(token, gistId);
+	console.log(gist)
 	return gist.files[GIST_FILE_NAME].content;
 }
 
